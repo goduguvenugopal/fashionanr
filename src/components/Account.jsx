9dd73cfba9f0c3a9328859da9fbcb0983cacee3b
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-06-01 18:29:31 +0530

    add

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -67,6 +67,7 @@ const Account = () => {
             <div className='row bg-white'>
                 <div className='col-6 col-md-3 bg-white'>
                     <div className='order-card-profile'>
+                        
                         <i class="fa-solid fa-box order-icon"></i>
                         <h5 className='bg-white orders-text-pro'>Orders</h5>
 
