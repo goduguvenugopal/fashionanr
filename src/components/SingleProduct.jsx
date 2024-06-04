80afe55cb248c77b0e0523179ca51e9f2791970a
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-06-04 10:21:42 +0530

    add

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -167,7 +167,7 @@ const SingleProduct = () => {
                     <div className='row'>
                         <div className='col-12 col-md-6 bg-white text-center pb-4 '>
 
-                            {data.image && (<img height="330px" width="320px" key={data._id} src={`${API}/${data.image}`} className='single-img bg-white' alt={data.category} />
+                            {data.image && (<img height="330px" width="320px" key={data._id} src={`${API}/uploads/${data.image}`} className='single-img bg-white' alt={data.category} />
                             )}
 
 
